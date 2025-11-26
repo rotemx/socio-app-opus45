e0abdaa58c042195265a59da85918afa10650dca
Rotem Meiri <rotemx@gmail.com>
2025-11-26 22:48:18 +0200

    fix: add missing jest dependencies and config to shared package

---
+++ packages/shared/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  transform: {
+    '^.+\.tsx?$': 'ts-jest',
+  },
+};
